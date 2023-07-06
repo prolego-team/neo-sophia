a65b8df9d592e56b2c60f2dcd0af437fa1ea9b4c
Justin Pounders <jmpounders@gmail.com>
2023-07-06 16:40:04 -0400

    added conversation ability

--- src/neosophia/llmtools/openaiapi.py
+++ src/neosophia/llmtools/openaiapi.py
@@ -2,9 +2,11 @@
 Wrappers for OpenAI API.
 """
 
-from typing import Any, List
+from typing import Any, List, Callable, Optional
 import os
 import sys
+from functools import partial
+from dataclasses import dataclass
 
 import openai as oai
 import torch
@@ -31,6 +33,12 @@ def load_api_key(file_path: str) -> str:
         sys.exit()
 
     return res
+
+
+def get_models_list() -> List:
+    """Return a list of available models."""
+    model_obj= oai.Model.list()
+    return [model.id for model in model_obj.data]
 
 
 def embeddings(texts: List[str]) -> Any:
@@ -67,3 +75,34 @@ def embeddings_tensor(texts: List[str]) -> torch.Tensor:
     res = extract_embeddings(embs)
     assert res.shape == (len(texts), EMBEDDING_DIM_DEFAULT)
     return res
+
+
+@dataclass
+class Message:
+    """Data structure for working with the messages that are passed back
+    and forth to the OpenAI chat APIs."""
+    role: str
+    content: str
+
+    def as_dict(self) -> dict:
+        """Because it's much, much faster than built in asdict."""
+        return vars(self)
+
+    @classmethod
+    def from_api_response(cls, response: dict):
+        """Parse the API response."""
+        role = response['choices'][0]['message']['role']
+        content = response['choices'][0]['message']['content']
+        return cls(role, content)
+
+
+def start_chat(model: str) -> Callable:
+    """Make an LLM interface function that you can use with Messages."""
+
+    def chat_func(messages: List[Message], *args, **kwargs) -> Message:
+        input = [message.as_dict() for message in messages]
+        response = oai.ChatCompletion.create(messages=input, model=model, *args, **kwargs)
+        return Message.from_api_response(response)
+    
+    return chat_func
+
