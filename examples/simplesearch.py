3bc57c065d08a44fa557e5dc3e2a4d54a26f98a2
Ben Zimmer <ben@prolego.io>
2023-07-06 13:23:34 -0500

    update

--- examples/simplesearch.py
+++ examples/simplesearch.py
@@ -1,12 +1,10 @@
 """
-Very basic semantic search example. Mainly used for verifying dependencies.
-We can get rid of this when we have a more relevant text to search.
+Very basic semantic search / question answering example.
 
-Expects the `hammurabi_openai.hf` dataset to be in the dataset directory.
 """
 
 # Copyright (c) 2023 Prolego Inc. All rights reserved.
-# Ben Zimmer
+
 import os
 import pickle
 import readline  # replaces `input` with an improved version
@@ -16,9 +14,8 @@ from typing import Dict, List
 import tqdm
 import torch
 import numpy as np
-import datasets as hfd
-import langchain
-import langchain.llms
+import openai
+
 
 import neosophia.llmtools.util as util
 
@@ -30,33 +27,25 @@ QUIT_KEYWORDS = ['q', 'quit', 'x', 'exit']
 OPENAI_LLM_MODEL_NAME = 'gpt-4'
 
 
-
-
-def build_qa_chain(
-        llm: langchain.llms.BaseLLM,
-        verbose: bool
-        ) -> langchain.LLMChain:
-    """build the LLMChain for answering questions given countext"""
-
-    # prompt based on langchain.stuff_prompt
-
-    prompt = langchain.PromptTemplate(
-        template=(
-            'Use the following pieces of context to answer the question at the end. ' +
-            # "If you don't know the answer, just say that you don't know,
-            # don't try to make up an answer. ' +
-            '\n\n{context}\n\n' +
-            'Question: {question}\n' +
-            'Helpful Answer:'
-        ),
-        input_variables=['context', 'question']
+def qa_func(context: str, question: str) -> str:
+    prompt = (
+        'Use the following pieces of context to answer the question at the end. ' +
+        # "If you don't know the answer, just say that you don't know,
+        # don't try to make up an answer. ' +
+        f'\n\n{context}\n\n' +
+        f'Question: {question}\n' +
+        'Helpful Answer:'
     )
 
-    return langchain.LLMChain(
-        prompt=prompt,
-        llm=llm,
-        verbose=verbose
+    res = openai.ChatCompletion.create(
+        model=OPENAI_LLM_MODEL_NAME,
+        messages=[
+            {'role': 'system', 'content': 'You are a helpful assistant.'},
+            {'role': 'user', 'content': prompt}
+        ]
     )
+
+    return res['choices'][0]['message']['content']
 
 
 def main() -> int:
@@ -64,6 +53,7 @@ def main() -> int:
 
     # configure stuff
     api_key = oaiapi.load_api_key(project.OPENAI_API_KEY_FILE_PATH)
+    oaiapi.set_api_key(api_key)
 
     with open('embeddings.pkl', 'rb') as f:
         records = pickle.load(f)
@@ -78,28 +68,21 @@ def main() -> int:
 
     def _level(name: str) -> int:
         """find the level of the rule"""
-        return len([x for x in name if x == '\'']) / 2
+        return len([x for x in name if x == '\'']) // 2
 
-    # keep top level rules
-    # rules = [
-    #     rule for rule in rules
-    #     if _level(rule['name']) == 1
-    # ]
+    # keep top level rules only
+    rules = [
+        rule for rule in rules
+        if _level(rule['name']) == 1
+    ]
 
     # for x in rules:
     #     print(x['name'])
     #     print('--------')
 
-    openai_llm = langchain.OpenAI(
-        openai_api_key=api_key,
-        model_name=OPENAI_LLM_MODEL_NAME
-    )
-
-    qa_chain = build_qa_chain(llm=openai_llm, verbose=True)
-
     while True:
 
-        search_str = input('Search string > ')
+        search_str = input('Question > ')
         if not search_str or search_str in QUIT_KEYWORDS:
             return 0
 
@@ -110,19 +93,22 @@ def main() -> int:
         # perform a very simple vector search
         rule_idxs = find_most_similar_idxs(rules, search_emb, MAX_RULES)
 
+        print('potentially relevant rules:\n')
+        for idx in rule_idxs:
+            print(rules[idx]['name'])
+            print(rules[idx]['text'])
+            print('~~~~ ~~~~ ~~~~ ~~~~')
+
         # find the rule_text and create context
         rule_text = [rules[idx]['text'] for idx in rule_idxs]
         context = '\n\n'.join(rule_text)
 
         # ask the question and get an answer
-        answer = qa_chain.run(context=context, question=search_str)
+        # answer = qa_chain.run(context=context, question=search_str)
+        answer = qa_func(context=context, question=search_str)
 
-        #for idx in rule_idxs:
-        #    print(rules[idx]['name'])
-        #    print(rules[idx]['text'])
-        #    print('~~~~ ~~~~ ~~~~ ~~~~')
         print('answer:', answer)
-        #print()
+        print()
 
 
 def find_most_similar_idxs(records: List[Dict], emb: torch.Tensor, n: float) -> List[int]:
@@ -132,7 +118,6 @@ def find_most_similar_idxs(records: List[Dict], emb: torch.Tensor, n: float) -> 
         score = torch.sum((emb - record['emb']) ** 2)
         scores.append(score.item())
     return np.argsort(scores)[:n]
-
 
 
 if __name__ == '__main__':
