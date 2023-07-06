3df8635e9a47698610a6d4d25e693f7b83b3b60f
Cameron Fabbri <cameronfabbri@gmail.com>
2023-07-06 14:01:57 -0600

    initial working version of the interface

--- examples/interface.py
+++ examples/interface.py
@@ -14,16 +14,20 @@ import datasets as hfd
 import langchain
 import langchain.llms
 
+import examples.simplesearch as ss
 import neosophia.llmtools.util as util
 
 from examples import project
 from neosophia.llmtools import openaiapi as oaiapi
 
+MAX_RULES = 5
+
 
 def setup():
     """ Configuration and data loading """
 
-    api_key = oaiapi.load_api_key(project.OPENAI_API_KEY_FILE_PATH)
+    api_key = oaiapi.load_api_key(project.OPENAI_API_KEY_FILE_PATH).rstrip()
+    oaiapi.set_api_key(api_key)
 
     with open('embeddings.pkl', 'rb') as f:
         records = pickle.load(f)
@@ -39,91 +43,86 @@ def setup():
     return records, rules
 
 
-def find_most_similar_idxs(
-        records: List[Dict], emb: torch.Tensor, n: float) -> List[int]:
-    """Simplest vector search implementation that performs a linear search."""
-    scores = []
-    for idx, record in tqdm.tqdm(enumerate(records)):
-        score = torch.sum((emb - record['emb']) ** 2)
-        scores.append(score.item())
-    return np.argsort(scores)[:n]
-
-
-def rule_lookup(prompt):
-
-    # get embedding of search string from OpenAI
-    search_emb = oaiapi.extract_embeddings(
-        oaiapi.embeddings([prompt]))[0]
-
-    # perform a very simple vector search
-    rule_idxs = find_most_similar_idxs(rules, search_emb, MAX_RULES)
-
-    # find the rule_text and create context
-    rule_text = [rules[idx]['text'] for idx in rule_idxs]
-    context = '\n\n'.join(rule_text)
-
-    # ask the question and get an answer
-    answer = qa_chain.run(context=context, question=question)
-
-    print('answer:', answer)
-
-
-def build_rules_html(records, rules: List) -> str:
-
-    rule_dict = {}
-
-    for r in records:
-
-        rule_name = r['rule_name']
-        section_label = r['section_label']
-        text = r['text']
-
-        if rule_name not in rule_dict:
-            rule_dict[rule_name] = {}
-
-        if section_label not in rule_dict[rule_name]:
-            rule_dict[rule_name][section_label] = text
-
-    html = '<html>\n<body>'
-    for rule_name, rule_sections in rule_dict.items():
-        indent_level = 0
-        html += f'<h2>{rule_name}</h2><br>'
-        for section_labels, text in rule_sections.items():
-            if None not in section_labels:
-                indent_level = len(section_labels)
-                section_labels = list(section_labels)[-1]
-            else:
-                section_labels = ''
-
-            indent = ''.join(['  '] * indent_level)
-
-            text = '\n'.join(textwrap.wrap(text, subsequent_indent=indent))
-            html += f'<pre>{indent}<b>{section_labels})</b></pre> '
-            html += f'<pre>{indent}{text}</pre>'
-
-    html += '</body>\n</html>'
-    with open('rules.html', 'w') as f:
-        f.write(html)
-
-    return html
+def _level(name: str) -> int:
+    """find the level of the rule"""
+    return len([x for x in name if x == '\'']) // 2
 
 
 def main():
 
-    records, rules = setup()
-    rules_html = build_rules_html(records, rules)
-    exit()
+    records, _rules = setup()
+    _rules = [
+        rule for rule in _rules
+        if _level(rule['name']) == 1
+    ]
+
+    rules = []
+    for rule in _rules:
+        rule_name = ' '.join(rule['name'].split()[:2])
+        section_id = rule['name'].split()[2:][0]
+        section_id = section_id.replace(
+            '(', '').replace(')', '').replace(',', '').replace('\'', '')
+
+        rules.append(
+            {
+                'name': rule_name + ' — ' + section_id,
+                'text': rule['text'],
+                'emb': rule['emb']
+            }
+        )
+
+    def semantic_search(search_str):
+
+        # get embedding of search string from OpenAI
+        search_emb = oaiapi.extract_embeddings(
+            oaiapi.embeddings([search_str]))[0]
+
+        # perform a very simple vector search
+        rule_idxs = ss.find_most_similar_idxs(rules, search_emb, MAX_RULES)
+
+        # find the rule_text and create context
+        rule_text = [
+            rules[idx]['name'] + ': \n' + rules[idx]['text'] for idx in rule_idxs]
+        context = '\n\n'.join(rule_text)
+
+        rule_names = ', '.join([rules[idx]['name'] for idx in rule_idxs])
+
+        rule_names = 'Relevant Rules: ' + rule_names
+
+        return rule_names, context
+
+    def format_search(search_str):
+        rule_names, context = semantic_search(search_str)
+        return 'Relevant Rules: ' + ''.join(rule_names) + '\n\n' + context
+
+    def ask_question(question: str):
+        # ask the question and get an answer
+        _, context = semantic_search(question)
+        answer = ss.qa_func(context=context, question=question)
+        output = ['Answer: ' + answer + '\n\n']
+        output.append(context)
+        return ''.join(output)
 
     with gr.Blocks() as demo:
-        gr.Markdown("# Welcome")
+        gr.Markdown('# Semantic Search')
         with gr.Row():
             with gr.Column():
-                text_input = gr.Textbox()
-                text_button = gr.Button('Search')
+                question_input = gr.Textbox()
+                question_button = gr.Button('Ask a question', scale=None)
+        with gr.Row():
             with gr.Column():
-                text_output = gr.Textbox()
+                search_input = gr.Textbox()
+                search_button = gr.Button('Search Rules', scale=None)
 
-        text_button.click(rule_lookup, inputs=text_input, outputs=text_output)
+        gr.Markdown("# Results")
+        with gr.Row():
+            text_output = gr.Textbox()
+
+        question_button.click(
+            ask_question, inputs=question_input, outputs=text_output)
+
+        search_button.click(
+            format_search, inputs=search_input, outputs=text_output)
 
     demo.launch()
 
