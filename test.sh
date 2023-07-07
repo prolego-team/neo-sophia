13788665a50adccb2635c0451dd26ac427c1eb50
Ben Zimmer <ben@prolego.io>
2023-07-07 11:54:21 -0500

    update test script

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@
 pytest -s --tb=short \
     --cov-report term-missing --cov-report html:htmlcov/tests \
     --cov=src/neosophia/llmtools \
-    src/neosophia
+    --cov=src/neosophia/db \
+    --cov=src/neosophia/datasets \
+    src/neosophia \
 
 if [ "$(uname)" == "Darwin" ]; then
     open htmlcov/tests/index.html
