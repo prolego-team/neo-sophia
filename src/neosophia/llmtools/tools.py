89383a1d3a4163ca06472217149d9fa0cc2b98fc
Justin Pounders <jmpounders@gmail.com>
2023-07-26 10:25:16 -0400

    wip sqlite tool

---
+++ src/neosophia/llmtools/tools.py
@@ -0,0 +1,49 @@
+"""
+A collection of tools that LLMs can use to solve problems.
+
+A tool is a function with a standard description.
+"""
+import sqlite3
+
+from ..db.sqlite_utils import get_db_creation_sql
+
+
+class SQLiteQueryTool:
+    """Run a sqlite query against a given database connection."""
+
+    def __init__(self, db_connection: sqlite3.Connection):
+        self._connection = db_connection
+
+        self._description = \
+        'Function for running a sqlite query against a databse.'
+        self._params = {
+            'query': {
+                'description': 'The query to run',
+                'typ': str,
+                'required': True
+            }
+        }
+
+    def __call__(self, query: str) -> list[tuple]:
+        """Run the query against the database connection.
+        
+        Returns a list of tuples"""
+        cursor = self._connection.cursor()
+        results = cursor.execute(query).fetchall()
+        return results
+
+
+# def make_sqlite_query_tool(db_connection: sqlite3.Connection) -> Callable:
+#     """"""
+#     def sqlite_tool(query: str):
+#         cursor = db_connection.cursor()
+#         results = cursor.execute(query).fetchall()
+#         return results
+
+#     return sqlite_tool
+
+
+conn = sqlite3.connect('synthbank.db')
+print(get_db_creation_sql(conn))
+tool1 = SQLiteQueryTool(conn)
+# tool2 = make_sqlite_query_tool(conn)
