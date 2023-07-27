f949f28853b35fd18397e5e52d6f2b285f193e2f
Ben Zimmer <ben@prolego.io>
2023-07-27 16:47:26 -0500

    update runner

--- examples/run_dispatch.py
+++ examples/run_dispatch.py
@@ -29,41 +29,77 @@ def main():
         'What is the current date and time?'
     ]
 
-    for question in questions:
-
-        print(question)
-
-        res = dp.dispatch_prompt_llm(
+    dispatchers = {
+        'OpenAI - Custom Prompt': lambda q, f: dp.dispatch_prompt_llm(
             llm=lambda x: oaiapi.chat_completion(x, model='gpt-4'),
-            question=question,
-            functions=functions
-        )
-
-        print()
-
-        if res is not None:
-            name, params = res
-            print('function name:', name)
-            print('function params:', params)
-            print()
-            print()
-        else:
-            print('No response')
-
-        res = dp.dispatch_openai_functioncall(
+            question=q,
+            functions=f
+        ),
+        'OpenAI - Function Calling': lambda q, f: dp.dispatch_openai_functioncall(
             model='gpt-4',
-            question=question,
-            functions=functions
+            question=q,
+            functions=f
         )
+    }
 
-        if res is not None:
-            name, params = res
-            print('function name:', name)
-            print('function params:', params)
-            print()
-            print()
-        else:
-            print('No response')
+    results = {}
+
+    for question in questions:
+        for disp_name, dispatcher in dispatchers.items():
+
+            print(question)
+
+            import time
+            start_time = time.time()
+
+            res = dispatcher(question, functions)
+
+            end_time = time.time()
+
+            total_time = end_time - start_time
+
+            info = {
+                'time': round(total_time, 3),
+            }
+
+            if res is not None:
+                name, params = res
+                print('function name:', name)
+                print('function params:', params)
+                print()
+                print()
+                info['missing'] = False
+                info['name'] = name
+                info['params'] = params
+            else:
+                print('No response')
+                info['missing'] = True
+                info['name'] = None
+                info['params'] = None
+
+            results[(disp_name, question)] = info
+
+    # save results as CSV
+
+    output_file_name = 'dispatch.csv'
+    header = 'dispatcher,question,time,missing,function,params\n'
+
+    with open(output_file_name, 'w') as f:
+        f.write(header)
+        for rid, info in results.items():
+            line = [
+                f'"{rid[0]}"',
+                f'"{rid[1]}"',
+                info['time'],
+                info['missing'],
+                info['name'],
+                f"\"{info['params']}\""
+            ]
+            line = [str(x) for x in line]
+            line = ','.join(line) + '\n'
+            f.write(line)
+
+    print(f'wrote `{output_file_name}`')
 
 
 if __name__ == '__main__':
