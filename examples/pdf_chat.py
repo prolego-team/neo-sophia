2dc03cf0cc6bcb5ae10cd614c0f8b09ce8762968
Cameron Fabbri <cameronfabbri@gmail.com>
2023-07-27 15:13:57 -0400

    added functions to return a string for an LLM

--- examples/pdf_chat.py
+++ examples/pdf_chat.py
@@ -20,12 +20,19 @@ from examples import project
 from neosophia.llmtools import openaiapi as oaiapi, pdf_utils, text_utils
 from neosophia.db.pdfdb import PDFDB
 
-CHROMADB_PERSIST_DIR = '.chroma_cache'
 MAX_TOKENS = {
     'gpt-4': 8192,
     'gpt-3.5-turbo': 4096,
     'gpt-3.5-turbo-16k': 16385
 }
+
+
+def format_page_res(context):
+    """ """
+    prompt = f"Filename: {context['filename']}\n"
+    prompt += f"Page Number: {context['page_num']}\n"
+    prompt += f"Text: {context['text']}"
+    return prompt
 
 
 @click.command()
@@ -41,49 +48,49 @@ def main(llm, data_dir):
     api_key = oaiapi.load_api_key(project.OPENAI_API_KEY_FILE_PATH)
     oaiapi.set_api_key(api_key)
 
-    question = 'When does an obligation qualify as a Type I security?'
-    question_emb = oaiapi.extract_embeddings(oaiapi.embeddings(question))[0]
-
-    filename = 'data/Title-12-Small/out.pdf'
+    # Create database
     pdfdb = PDFDB('pdf_db')
-    pdfdb.add_pdf(filename)
 
+    #filename1 = 'data/Title-12-Small/out.pdf'
+    filename1 = 'data/Title-12/Title-12-Volume-1.pdf'
+    filename2 = 'data/2306.05284.pdf'
+    #pdfdb.add_pdf(filename1)
+    #pdfdb.add_pdf(filename2)
+
+    question = 'What is the difference between a Type I and Type II security?'
+    result = pdfdb.get_context(question)
+
+    '''
+    out = pdfdb.section_collection.query(
+        query_embeddings=[query_embedding.tolist()],
+        n_results=4,
+    )
+
+    page = pdfdb.get_page(filename=filename1, page_num=1)
     out = pdfdb.get_section(
-        filename=filename, page_num=0, section_id=0)
-
-    print('OUT:', out)
-    exit()
-
+        filename=filename1, page_num=0, section_id=0)
     res = pdfdb.section_collection.query(
         query_embeddings=[question_emb.tolist()], n_results=4)
+    '''
 
-    pages = []
-    for rid in res['ids'][0]:
-        _, page_num, _ = rid.split('~')
-        if page_num not in pages:
-            pages.append(page_num)
+    for res in result:
 
-    for page_num in pages:
-
-        page_res = pdfdb.page_collection.get(
-            filename + '~' + str(page_num))
-
-        text = page_res['documents'][0]
         prompt = 'Determine if the following context has enough information to answer the question. If it doesn\'t, say "There is not enough context to provide an answer". If it does, provide the answer. THINK STEP BY STEP. ALWAYS PROVIDE QUOTES AND PAGE CITATIONS.\n\n'
         prompt += f'Question: {question}\n'
-        prompt += f'Context: \n{text}'
+
+        context = format_page_res(res)
+        prompt += f'Context: \n{context}'
 
         out = oaiapi.chat_completion(
             prompt=prompt,
             model=llm)
 
         if 'There is no' in out:
-            print(f'Not enough information on page', str(page_num))
+            print(f'Not enough information on page', str(res['page_num']))
         else:
-            print('Answer found on page', page_num)
+            print('Answer found on page', res['page_num'])
             print(out)
             print('\n-----------------------------------------------------------\n')
-            break
 
 
 if __name__ == '__main__':
--- src/neosophia/db/pdfdb.py
+++ src/neosophia/db/pdfdb.py
@@ -2,8 +2,13 @@
 """
 import os
 
+from typing import List
+from dataclasses import dataclass
+
+import torch
 import chromadb
 
+from tqdm import tqdm
 from chromadb.utils import embedding_functions
 
 import neosophia.db.chroma as chroma
@@ -11,6 +16,10 @@ import neosophia.db.chroma as chroma
 from neosophia.llmtools import openaiapi as oaiapi, pdf_utils, text_utils
 
 EMBEDDING_MODEL = 'text-embedding-ada-002'
+
+MAX_INPUT_TOKENS = {
+    'text-embedding-ada-002': 8191
+}
 
 
 class PDFDB:
@@ -25,6 +34,11 @@ class PDFDB:
                         model_name=EMBEDDING_MODEL
         )
 
+        self.pdf_collection = self.chroma_client.get_or_create_collection(
+            name='pdf_collection',
+            embedding_function=self.openai_ef
+        )
+
         self.page_collection = self.chroma_client.get_or_create_collection(
             name='page_collection',
             embedding_function=self.openai_ef
@@ -35,38 +49,93 @@ class PDFDB:
             embedding_function=self.openai_ef
         )
 
-    def add_pdf(self, filename):
+    def search_by_embedding(self, query: str, n_results: int):
+        query_embedding = oaiapi.extract_embeddings(oaiapi.embeddings(query))[0]
+        return self.section_collection.query(
+            query_embeddings=[query_embedding.tolist()], n_results=n_results)
+
+    def get_context(self, query: str):
         """ """
+
+        res = self.search_by_embedding(query, n_results=8)
+
+        pages = []
+        for rid in res['ids'][0]:
+            filename, page_num, _ = rid.split('~')
+            if page_num not in pages:
+                pages.append(page_num)
+
+        data = []
+        for page_num in pages:
+            page_res = self.page_collection.get(
+                filename + '~' + str(page_num))
+            data.append(
+                {
+                    'text': page_res['documents'][0],
+                    'filename': filename,
+                    'page_num': page_num
+                }
+            )
+
+        return data
+
+    def add_pdf(self, filename):
+        """
+        Adds a PDF if it doesn't exist in the database. If it does exist,
+        update it
+
+        General rule of thumb is 1 token per 4 characters of text
+        ref: https://platform.openai.com/tokenizer
+
+        1. Extract flat text from PDF into a list of pages
+        2. Create an embedding for each page
+        section
+        3. Split pages into sections and create an embedding for each section
+        4. Split entire PDF into sections of length `max_chars`
+        6. Create an embedding for each page, then average the embeddings to
+        create a full document embedding
+        6. Another option would be to recursively split and summarize the
+        document into chunks of size `max_chars`, then embed the final summary
+        """
 
         if not os.path.isfile(filename):
             raise ValueError('File not found')
 
+        print(f'Adding {filename} to database...')
+
         # List of text for each page in the PDF
-        pdf_text = pdf_utils.extract_text_from_pdf(filename)
+        page_texts = pdf_utils.extract_text_from_pdf(filename)
 
-        page_data_dict = {}
-        section_data_dict = {}
-        for page_num, page_text in enumerate(pdf_text):
+        '''
+        # Maximum number of characters we can send to the embedding model
+        max_chars = MAX_INPUT_TOKENS['text-embedding-ada-002'] * 4
+        print('max_chars:', max_chars)
 
-            # Add the embedding for the whole page
-            page_data_dict[f'{page_num}'] = {
-                'text': page_text,
-                'embedding': oaiapi.extract_embeddings(
-                    oaiapi.embeddings(page_text))[0]
-            }
+        all_text = ' '.join([pt for pt in page_texts])
+        print('all_text:', len(all_text))
 
-            # Split the page into 4 parts and add all those embeddings
-            page_sections = text_utils.split_text_into_sections(page_text, 4)
-            for sidx, text in enumerate(page_sections):
-                section_data_dict[f'{page_num}-{sidx}'] = {
-                    'text': text,
-                    'embedding': oaiapi.extract_embeddings(
-                        oaiapi.embeddings(text))[0]
-                }
+        num_large_sections = int(len(all_text) / max_chars) + 1
+        print('num_large_sections:', num_large_sections)
+
+        large_sections = text_utils.split_text_into_sections(
+            all_text, num_large_sections)
+
+        prompt = 'Give me the TLDR:\n\n'
+        summarized_sections = []
+        for text in tqdm(large_sections):
+            summarized_sections.append(
+                oaiapi.chat_completion(
+                    prompt=prompt + text,
+                    model='gpt-4'))
+        summarized_sections = '\n'.join(summarized_sections)
+        '''
+
+        # Get page and section embeddings
+        page_data, section_data_dict = text_utils.create_page_embeddings(
+            page_texts)
 
         # Add whole pages to collection
-        for page_num, data in page_data_dict.items():
-
+        for page_num, data in enumerate(page_data):
             self.page_collection.upsert(
                 embeddings=[data['embedding'].tolist()],
                 documents=[data['text']],
@@ -78,6 +147,25 @@ class PDFDB:
                 ],
                 ids=[filename + '~' + str(page_num)]
             )
+
+        # Compute the average embedding across all sections
+        pdf_embedding = torch.mean(
+            torch.cat(
+                [
+                    torch.unsqueeze(
+                        x['embedding'], 0
+                    ) for x in section_data_dict.values()
+                ]),
+            axis=0
+        )
+
+        # Add full pdf to collection
+        self.pdf_collection.upsert(
+            embeddings=[pdf_embedding.tolist()],
+            documents=['\n'.join(page_texts)],
+            metadatas=[{'filename': filename}],
+            ids=[filename]
+        )
 
         # Add sections to collection
         for page_id, data in section_data_dict.items():
@@ -95,20 +183,23 @@ class PDFDB:
                 ids=[filename + '~' + str(page_num) + '~' + str(section_id)]
             )
 
+    def get_pdf(self, filename):
+        return self.pdf_collection.get(filename)
+
+    def get_page(self, filename, page_num, include_embeddings=False):
+        include = ['documents', 'metadatas']
+        if include_embeddings:
+            include.append('embeddings')
+        return self.page_collection.get(
+            filename + '~' + str(page_num), include=include)
+
     def get_section(
             self, filename, page_num, section_id, include_embeddings=False):
         include = ['documents', 'metadatas']
         if include_embeddings:
             include.append('embeddings')
         return self.section_collection.get(
-            filename + '-' + str(page_num) + '-' + str(section_id), include)
-
-    def get_all_pdfs(self):
-        return self.db.find_all(self.collection_name)
-
-    def delete_pdf(self, filename):
-        query = {'filename': filename}
-        self.db.delete(self.collection_name, query)
-
+            filename + '~' + str(page_num) + '~' + str(section_id),
+            include=include)
 
 
