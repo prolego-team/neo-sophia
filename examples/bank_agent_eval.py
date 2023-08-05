98a0ba81faf8fb42031c304d71aa27d9e4e41c34
Ben Zimmer <ben@prolego.io>
2023-08-04 20:51:48 -0500

    update

--- examples/bank_agent_eval.py
+++ examples/bank_agent_eval.py
@@ -2,20 +2,13 @@
 Test the bank agent with questions and answers.
 """
 
-import os
-import sys
-import logging
+from typing import Iterable, Optional
 import sqlite3
-from datetime import datetime
-
-from typing import Optional
-
-
-import gradio as gr
+import tqdm
+import time
 
 from neosophia.llmtools import openaiapi as openai, tools
 from neosophia.agents.react import make_react_agent
-from neosophia.db.sqlite_utils import get_db_creation_sql
 
 from examples import bank_agent as ba
 
@@ -27,6 +20,9 @@ DATABASE = 'data/synthbank.db'
 
 def main():
     """main program"""
+
+    # configuration
+    n_runs = 3
 
     # setup
     api_key = oaiapi.load_api_key(project.OPENAI_API_KEY_FILE_PATH)
@@ -45,40 +41,120 @@ def main():
         'query_database': query_database
     }
 
-    agent = make_react_agent(
-        system_message, model, function_descriptions, functions)
+    # TODO: additional system functions to evaluate
 
-    # question = 'Who has the most money?'
-    question = 'Who has the most money in their checking account?'
+    def agent_simple(question: str) -> Optional[str]:
+        """answer a question with the simple agent"""
+        agent = make_react_agent(
+            system_message, model, function_descriptions, functions,
+            ba.MAX_LLM_CALLS_PER_INTERACTION, True)
+        return find_answer(agent(question))
 
+    def agent_react(question: str) -> Optional[str]:
+        """answer a question with the react agent"""
+        agent = make_react_agent(
+            system_message, model, function_descriptions, functions,
+            ba.MAX_LLM_CALLS_PER_INTERACTION, False)
+        return find_answer(agent(question))
+
+    systems = {
+        'agent (simple)': agent_simple,
+        # 'agent (react)': agent_react)
+    }
+
+    qs_and_evals = [
+        ('Who most recently opened a checking account?', lambda x: 'John Thompson' in x),
+        ('How many people have opened a savings account in the last year?', lambda x: '34' in x.split('\\s+')),
+        ('How many products does the person who most recently opened a mortgage have?', lambda x: '2' in x.split('\\s+')),
+        (
+            'Which customer has the highest interest rate on their credit card, and what is that interest rate?',
+            lambda x: ('Edith Nelson' in x or '77' in x) and (('0.3' in x.split('\\s+') or '30%' in x.split('\\s+')))
+        )
+    ]
+
+    results = {}
+
+    for question, eval_func in tqdm.tqdm(qs_and_evals):
+        for system_name, system in systems.items():
+            for run_idx in range(n_runs):
+
+                # use the system to answer the question
+                start_time = time.time()
+                answer = system(question)
+                print('ANSWER:')
+                print(answer)
+                end_time = time.time()
+                total_time = end_time - start_time
+
+                info = {
+                    'time': round(total_time, 3),
+                    'answer': answer
+                }
+
+                # evaluation
+                if answer is not None:
+                    info['missing'] = False
+                    info['correct'] = eval_func(answer)
+                else:
+                    info['missing'] = True
+                    info['correct'] = False
+
+                results[(system_name, question, run_idx)] = info
+
+    # save results as CSV
+
+    output_file_name = 'eval.csv'
+    header = 'system,question,run,time,missing,correct,answer\n'
+
+    with open(output_file_name, 'w') as f:
+        f.write(header)
+        for (system_name, question, run_idx), info in results.items():
+            answer = info['answer']
+            answer = answer.replace('\n', '%%%').replace('"', '``')
+            line = [
+                f'"{system_name}"',
+                f'"{question}"',
+                run_idx,
+                info['time'],
+                info['missing'],
+                info['correct'],
+                f'"{answer}"'
+            ]
+            line = [str(x) for x in line]
+            line = ','.join(line) + '\n'
+            f.write(line)
+
+    print(f'wrote `{output_file_name}`')
+
+    db_connection.close()
+
+    # TODO: do some aggregation of results across runs
+
+
+def find_answer(messages: Iterable[openai.Message]) -> Optional[str]:
+    """
+    Consume messages from an agent until you find 'Final Answer:'
+    from the assistant.
+
+    Note: I've seen the assistant say things like "I don't have
+    enough information to answer the question, but if you give
+    me the right information, I'll be able to answer and I'll make
+    sure to begin my answer with 'Final Answer:'". LOLOLOL.
+    """
     answer_message = None
-    for message in agent(question):
+    for message in messages:
+        print("MESSAGE")
         print(message)
+        print('----')
         if message.role == 'assistant':
             if 'Final Answer:' in message.content:
                 answer_message = message
                 break
-
-    print('FINAL ANSWER:')
+        print('continuing')
     if answer_message is not None:
-        print(answer_message.content)
+        return answer_message.content
     else:
-        print('UNABLE TO ANSWER THE QUESTION')
-
-    db_connection.close()
-
-
-# def find_answer(chat_history) -> Optional[str]:
-#     final_message = chat_history[-1][1]
-#     if final_message is None:
-#         response = None
-#     elif 'Final Answer:' in final_message:
-#         response = final_message.split('Final Answer:')[1].strip()
-#         response = response.split('\n')[0].strip()
-#     else:
-#         response = None
-#
-#     return response
+        return None
 
 
 if __name__ == '__main__':
